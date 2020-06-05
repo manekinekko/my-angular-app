daa9867e6a6e71819ce32bee7ff556e2a8384e36
Wassim Chegham <github@wassim.dev>
2020-06-05 17:32:43 +0200

    feat: add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
